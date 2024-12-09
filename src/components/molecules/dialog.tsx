e9714f732910792bd48e3cfa0b42f4c0b23562e4
Tony Ko <tony@kohei.io>
2024-12-09 02:50:49 -0500

    fix: lightbox close button pos

--- src/components/molecules/dialog.tsx
+++ src/components/molecules/dialog.tsx
@@ -14,7 +14,7 @@ const styles = {
   dialogContent: cva(
     'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg'
   ),
-  dialogClose: cva('absolute right-4 top-5'),
+  dialogClose: cva('absolute right-5 top-5'),
   dialogHeader: cva('flex flex-col space-y-1.5 text-center sm:text-left'),
   dialogFooter: cva('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2'),
   dialogTitle: cva('text-h3 font-alliance-no2 mb-2'),
